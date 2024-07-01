e5352528ba5fd111ba82889a15eaf3876ad6042e
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-01 15:06:08 +0100

    get and post requests done

--- src/server.js
+++ src/server.js
@@ -2,6 +2,40 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const fakeData = [
+  { id: 1, title: "book1", author: "author1", genre: "genre1" },
+  { id: 2, title: "book2", author: "author2", genre: "genre2" },
+  { id: 3, title: "book3", author: "author3", genre: "genre3" },
+];
+
+app.get("/books", (request, response) => {
+  //   console.log(request.path, ":", typeof request.path);
+  response.send("Hello from /books");
+});
+
+app.get("/books/getAllBooks", (request, response) => {
+  const successResponse = {
+    message: "success",
+    books: fakeData,
+  };
+
+  response.send(successResponse);
+});
+
+app.post("/books/addBook", (request, response) => {
+  console.log(request.body);
+  fakeData.push(request.body);
+
+  const successResponse = {
+    message: "success",
+    books: fakeData,
+  };
+
+  response.send(successResponse);
+});
+
 app.listen(5001, () => {
   console.log(`Server is listening on port 5001`);
 });
