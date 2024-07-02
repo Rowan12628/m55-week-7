139e7585a70bc74c8b0e01d0671503abc0b7ce77
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-02 10:03:59 +0100

    added del and update solutions

--- src/server.js
+++ src/server.js
@@ -36,6 +36,59 @@ app.post("/books/addBook", (request, response) => {
   response.send(successResponse);
 });
 
+app.put("/books", (request, reponse) => {
+  // in here, find a book by title (i.e. an element of fakeArr where the element title is the same as request.body.title)
+  // change (update) the author to an new name
+
+  console.log(request.body);
+  function findBook(x) {
+    return x.title === request.body.title;
+  }
+
+  const index = fakeData.findIndex(findBook);
+
+  if (index === -1) {
+    const failureResponse = {
+      message: `${request.body.title} not found`,
+    };
+    response.send(failureResponse);
+    return;
+  }
+
+  fakeData[index].title = request.body.newTitle;
+
+  const successResponse = {
+    message: "success",
+    books: fakeData[index],
+  };
+
+  response.send(successResponse);
+});
+
+app.delete("/books/delBooks", (request, response) => {
+  // in here, find a book by title (i.e. an element of fakeArr where the element title is the same as request.body.title)
+  // remove (delete) the element from the array
+
+  const index = fakeData.findIndex((book) => book.title === request.body.title);
+
+  if (index === -1) {
+    const failureResponse = {
+      message: `${resuest.body.title} not found`,
+    };
+    response.send(failureResponse);
+    return;
+  }
+
+  fakeData.splice(index, 1);
+
+  const successResponse = {
+    message: "success",
+    books: fakeData,
+  };
+
+  response.send(successResponse);
+});
+
 app.listen(5001, () => {
   console.log(`Server is listening on port 5001`);
 });
