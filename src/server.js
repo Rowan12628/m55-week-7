4c7607a8622670ee6ac1588f8f4d9fb39fb2ae00
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-03 16:55:05 +0100

    add addBook route

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -5,9 +6,50 @@ const app = express();
 
 app.use(express.json());
 
+const connection = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("db is working");
+};
+
+connection();
+
+//book model --
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+//-- book model ends
+
 app.get("/books/getAllBooks", (request, response) => {});
 
-app.post("/books/addBook", (request, response) => {});
+app.post("/books/addBook", async (request, response) => {
+  const book = await Book.create({
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  });
+
+  const successResponse = {
+    message: "success",
+    book: book,
+  };
+
+  response.send(successResponse);
+});
 
 app.put("/books/putBooks", (request, response) => {});
 
