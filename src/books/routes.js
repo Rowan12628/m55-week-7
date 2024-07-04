d2f97348e2f022f4d4accad825bf266e916b5ea0
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-04 14:24:33 +0100

    refactored putBooks into routes.js

--- src/books/routes.js
+++ src/books/routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const bookRouter = Router();
 
-// const Book = require("./model");
+const Book = require("./model");
 
 const { getAllBooks } = require("./controllers");
 const { addBook } = require("./controllers");
@@ -11,5 +11,19 @@ bookRouter.get("/books/getAllBooks", getAllBooks); //reference function, don't c
 
 //addBook
 bookRouter.post("/books/addBook", addBook);
+
+//putBooks
+bookRouter.put("/books/putBooks", async (request, response) => {
+  const updateBook = await Book.updateOne(
+    { title: request.body.title },
+    { $set: { author: request.body.author } }
+  );
+
+  const successResponse = {
+    message: "success",
+    updateBook: updateBook,
+  };
+  response.send(successResponse);
+});
 
 module.exports = bookRouter;
